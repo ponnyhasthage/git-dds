dbc225c636f4ceb8af5ac2e93d7ca2412903d019
Sofie Predikaka <sofie.predikaka@gmail.com>
2013-10-11 13:41:58 +0200

    Added js to the file main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 // Why hello there, sexy.
 // This is a javascript comment. Any line that begins with // will be like this.
 // Comments have NO effect on your webpage. NONE! ZERO!
+
+  <script type="text/javascript">
+    window.onload = function() {
+      var node = document.getElementById('blinking-text');
+      setInterval(function() {
+        visibility = node.style.visibility;
+        if (visibility == "") {
+          node.style.visibility = "hidden";
+        } else {
+          node.style.visibility = "";
+        }
+      }, 800);
+    }
+  </script>
